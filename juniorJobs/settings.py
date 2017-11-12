5d57fc35c6d11f382cfabb2acc5a3c5113cea841
remery426 <emeryryan1@gmail.com>
2017-11-12 11:38:24 -0800

    changed settings

--- juniorJobs/settings.py
+++ juniorJobs/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions.backends.db',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
