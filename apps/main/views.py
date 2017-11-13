31d64be58011f42ef90ab813b122f39f4dd6ed57
remery426 <emeryryan1@gmail.com>
2017-11-12 22:15:47 -0800

    normal with try

--- apps/main/views.py
+++ apps/main/views.py
@@ -6,7 +6,17 @@ from ..login.models import User
 from .models import Search
 
 def index(request):
-
+    try:
+        this_user = User.objects.filter(email = request.session['currentUser'])[0]
+        userSearchs = Search.objects.filter(userlist =this_user)
+        context = {
+                'userMessage' : "Welcome " + request.session['currentUser'],
+                'userSearch' : userSearchs
+                }
+    except:
+        context = {
+            'message' : 'Want to save your search history? Click here to login.'
+        }
     return render(request,'main/index.html',context)
 
 # Create your views here.
